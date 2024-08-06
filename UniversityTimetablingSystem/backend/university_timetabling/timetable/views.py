643f1a299d20c3b5281a36142cf873a94ea796ed
Zhanna Martirosyan <zhanna.martirosyan@student.rau.am>
2024-08-06 11:42:10 +0400

    subprocessinstead of pybind11

--- UniversityTimetablingSystem/backend/university_timetabling/timetable/views.py
+++ UniversityTimetablingSystem/backend/university_timetabling/timetable/views.py
@@ -6,20 +6,52 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import traceback
+import subprocess
 
-sys.path.append('/Users/apple/intership/UniversityTimetablingSystem/build/')
-try:
-    import university_bindings as ub
-except ImportError as e:
-    raise ImportError(f"Failed to import university_bindings: {e}")
-
-university = ub.University()
-university.loadState("result.json")
+# sys.path.append('/Users/apple/intership/UniversityTimetablingSystem/build/')
+# try:
+#     import university_bindings as ub
+# except ImportError as e:
+#     raise ImportError(f"Failed to import university_bindings: {e}")
+#
+# university = ub.University()
+# university.loadState("result.json")
 
 @csrf_exempt
 def add_instructor(request):
     if request.method == 'POST':
         try:
+            # data = json.loads(request.body)
+            # name = data.get('name')
+            # preferred_courses_data = data.get('preferred_courses', [])
+            # availability_data = data.get('availability', [])
+            #
+            # if not name:
+            #     return JsonResponse({'status': 'Error', 'message': 'Missing name'}, status=400)
+            #
+            # availability = [ub.TimeSlot(slot['day'], slot['startTime'], slot['endTime']) for slot in availability_data]
+            #
+            # preferred_courses = []
+            # for course_data in preferred_courses_data:
+            #     print(f"Processing course data: {course_data}")
+            #
+            #     course_name = course_data.get('courseName')
+            #
+            #     if not university.courseExists(course_name):
+            #         return JsonResponse({'status': 'Error', 'message': f'Course {course_name} does not exist'}, status=400)
+            #
+            #     try:
+            #         course = university.getCourse(course_name)
+            #     except Exception as e:
+            #         return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
+            #
+            #     preferred_courses.append(course)
+            #
+            # instructor = ub.Instructor(name, availability, preferred_courses)
+            # university.addInstructor(instructor)
+            #
+            # university.saveState('result.json')
+
             data = json.loads(request.body)
             name = data.get('name')
             preferred_courses_data = data.get('preferred_courses', [])
@@ -28,30 +60,23 @@ def add_instructor(request):
             if not name:
                 return JsonResponse({'status': 'Error', 'message': 'Missing name'}, status=400)
 
-            availability = [ub.TimeSlot(slot['day'], slot['startTime'], slot['endTime']) for slot in availability_data]
+            args = [
+                '/Users/apple/intership/UniversityTimetablingSystem/build/UniversityTimetablingSystem',
+                '--addInstructor',
+                json.dumps({
+                    'name': name,
+                    'preferred_courses': preferred_courses_data,
+                    'availability': availability_data
+                })
+            ]
 
-            preferred_courses = []
-            for course_data in preferred_courses_data:
-                print(f"Processing course data: {course_data}")
-
-                course_name = course_data.get('courseName')
-
-                if not university.courseExists(course_name):
-                    return JsonResponse({'status': 'Error', 'message': f'Course {course_name} does not exist'}, status=400)
-
-                try:
-                    course = university.getCourse(course_name)
-                except Exception as e:
-                    return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
-
-                preferred_courses.append(course)
-
-            instructor = ub.Instructor(name, availability, preferred_courses)
-            university.addInstructor(instructor)
-
-            university.saveState('result.json')
+            result = subprocess.run(args, capture_output=True, text=True, check=True)
+            print('Output:', result.stdout)
 
             return JsonResponse({'status': 'Instructor added successfully'})
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+            return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
         except Exception as e:
             print(f"Error occurred: {e}")
             return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
@@ -62,21 +87,43 @@ def add_instructor(request):
 def add_course(request):
     if request.method == 'POST':
         try:
-            data = json.loads(request.body)
+            # data = json.loads(request.body)
+            #
+            # name = data.get('name')
+            # preferred_time_slots_data = data.get('preferred_time_slots', [])
+            #
+            # if not name:
+            #     return JsonResponse({'status': 'Error', 'message': 'Missing name'}, status=400)
+            #
+            # preferred_time_slots = [ub.TimeSlot(slot['day'], slot['startTime'], slot['endTime']) for slot in preferred_time_slots_data]
+            #
+            # course = ub.Course(name, preferred_time_slots)
+            # university.addCourse(course)
+            # university.saveState('result.json')
 
+            data = json.loads(request.body)
             name = data.get('name')
             preferred_time_slots_data = data.get('preferred_time_slots', [])
 
             if not name:
                 return JsonResponse({'status': 'Error', 'message': 'Missing name'}, status=400)
 
-            preferred_time_slots = [ub.TimeSlot(slot['day'], slot['startTime'], slot['endTime']) for slot in preferred_time_slots_data]
+            args = [
+                '/Users/apple/intership/UniversityTimetablingSystem/build/UniversityTimetablingSystem',
+                '--addCourse',
+                json.dumps({
+                    'name': name,
+                    'preferred_time_slots': preferred_time_slots_data
+                })
+            ]
 
-            course = ub.Course(name, preferred_time_slots)
-            university.addCourse(course)
-            university.saveState('result.json')
+            result = subprocess.run(args, capture_output=True, text=True, check=True)
+            print('Output:', result.stdout)
 
             return JsonResponse({'status': 'Course added successfully'})
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+            return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
         except Exception as e:
             print(f"Error occurred: {e}")
             return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
@@ -86,22 +133,39 @@ def add_course(request):
 def add_time_slot(request):
     if request.method == 'POST':
         try:
+            # data = json.loads(request.body)
+            # if 'time_slots' not in data:
+            #     return JsonResponse({'status': 'Error', 'message': 'No time_slots in request body'}, status=400)
+            #
+            # for ts in data['time_slots']:
+            #     day = ts.get('day')
+            #     start_time = ts.get('startTime')
+            #     end_time = ts.get('endTime')
+            #
+            #     if not day or not start_time or not end_time:
+            #         return JsonResponse({'status': 'Error', 'message': 'Missing fields in time slot'}, status=400)
+            #
+            #     time_slot = ub.TimeSlot(day, start_time, end_time)
+            #     university.addTimeSlot(time_slot)
+            # university.saveState('result.json')
+
             data = json.loads(request.body)
             if 'time_slots' not in data:
                 return JsonResponse({'status': 'Error', 'message': 'No time_slots in request body'}, status=400)
 
-            for ts in data['time_slots']:
-                day = ts.get('day')
-                start_time = ts.get('startTime')
-                end_time = ts.get('endTime')
+            args = [
+                '/Users/apple/intership/UniversityTimetablingSystem/build/UniversityTimetablingSystem',
+                '--addTimeSlot',
+                json.dumps(data['time_slots'])
+            ]
 
-                if not day or not start_time or not end_time:
-                    return JsonResponse({'status': 'Error', 'message': 'Missing fields in time slot'}, status=400)
+            result = subprocess.run(args, capture_output=True, text=True, check=True)
+            print('Output:', result.stdout)
 
-                time_slot = ub.TimeSlot(day, start_time, end_time)
-                university.addTimeSlot(time_slot)
-            university.saveState('result.json')
             return JsonResponse({'status': 'Time Slot(s) added successfully'})
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+            return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
         except Exception as e:
             return JsonResponse({'status': 'Error', 'message': str(e)}, status=400)
     return JsonResponse({'status': 'Invalid request method'}, status=405)
@@ -110,22 +174,37 @@ def add_time_slot(request):
 def generate_schedule(request):
     if request.method == 'POST':
         try:
-            university = ub.University()
-            university.loadState('result.json')
+            # university = ub.University()
+            # university.loadState('result.json')
+            #
+            # university_map = university.schedule()
+            # schedule_data = university.scheduleToJsonFormat()
+            #
+            # if not schedule_data:
+            #     #print("Debug: schedule_data is empty")
+            #     return JsonResponse({'error': 'No schedule data available'}, status=500)
+            #
+            # university.saveState('result.json')
+            #
+            # schedule = [{'course': course, 'timeSlot': timeSlot, 'instructor': instructor}
+            #             for course, timeSlot, instructor in schedule_data]
 
-            university_map = university.schedule()
-            schedule_data = university.scheduleToJsonFormat()
+            args = [
+                '/Users/apple/intership/UniversityTimetablingSystem/build/UniversityTimetablingSystem',
+                '--schedule'
+            ]
 
-            if not schedule_data:
-                #print("Debug: schedule_data is empty")
+            result = subprocess.run(args, capture_output=True, text=True, check=True)
+            print('Output:', result.stdout)
+
+            schedule = json.loads(result.stdout)
+            if not schedule:
                 return JsonResponse({'error': 'No schedule data available'}, status=500)
 
-            university.saveState('result.json')
-
-            schedule = [{'course': course, 'timeSlot': timeSlot, 'instructor': instructor}
-                        for course, timeSlot, instructor in schedule_data]
-
             return JsonResponse({'schedule': schedule})
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+            return JsonResponse({'error': str(e)}, status=500)
         except Exception as e:
             traceback.print_exc()
             return JsonResponse({'error': str(e)}, status=500)
@@ -135,19 +214,32 @@ def generate_schedule(request):
 @csrf_exempt
 def show_university(request):
     try:
-        university = ub.University()
-        university.loadState('result.json')
+        # university = ub.University()
+        # university.loadState('result.json')
+        #
+        # courses = [{'name': course.getCourseName()} for course in university.getCourses()]
+        # instructors = [{'name': instructor.getName()} for instructor in university.getInstructors()]
+        # time_slots = [{'day': slot.getDay(), 'startTime': slot.getStartTime(), 'endTime': slot.getEndTime()} for slot in university.getTimeSlots()]
+        #
+        # return JsonResponse({
+        #     'courses': courses,
+        #     'instructors': instructors,
+        #     'timeSlots': time_slots
+        # })
 
-        courses = [{'name': course.getCourseName()} for course in university.getCourses()]
-        instructors = [{'name': instructor.getName()} for instructor in university.getInstructors()]
-        time_slots = [{'day': slot.getDay(), 'startTime': slot.getStartTime(), 'endTime': slot.getEndTime()} for slot in university.getTimeSlots()]
+        args = [
+            '/Users/apple/intership/UniversityTimetablingSystem/build/UniversityTimetablingSystem',
+            '--showUniversity'
+        ]
 
-        return JsonResponse({
-            'courses': courses,
-            'instructors': instructors,
-            'timeSlots': time_slots
-        })
+        result = subprocess.run(args, capture_output=True, text=True, check=True)
+        print('Output:', result.stdout)
 
+        university_data = json.loads(result.stdout)
+        return JsonResponse(university_data)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        return JsonResponse({'status': 'Error', 'message': str(e)}, status=500)
     except Exception as e:
         print("Exception occurred:", e)
         traceback.print_exc()
--- UniversityTimetablingSystem/src/main.cpp
+++ UniversityTimetablingSystem/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cstdio>
 
 #include "Course.h"
 #include "Instructor.h"
@@ -17,125 +18,221 @@ Course* findCourseByName(const std::string& courseName, University& university) 
     return nullptr;
 }
 
-int main(int argc, char** argv) {
-    argparse::ArgumentParser program("UniversityTimetablingSystem", "University");
+void processArgs(int argc, char** argv, University& university) {
+    std::string stateFile = "result.json";
 
-    // define arguments
-    program.add_argument("--addInstructor", "Add a new instructor", false);
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
 
-    program.add_argument("--addCourse", "Add a new course", false);
+        if (arg == "--stateFile" && i + 1 < argc) {
+            stateFile = argv[++i];
+        } else if (arg == "--addInstructor" && i + 1 < argc) {
+            university.loadState(stateFile);
+            std::string instructorJson = argv[++i];
+            json j = json::parse(instructorJson);
+            std::vector<Course> preferredCourses;
+            std::vector<TimeSlot> availability;
 
-    program.add_argument("--addTimeSlot", "Add a new time slot", false);
-
-    program.add_argument("--schedule", "Generate the schedule", false);
-
-    program.add_argument("--availability", "Represents instructor's availability", false);
-
-    program.add_argument("--preferredCourses", "Represents instructor's preferred courses", false);
-
-    program.add_argument("--preferredTimeSlots", "Represents preferred course time slots", false);
-
-    program.add_argument("--clearFile", "Clear the result.json file", false);
-
-    program.add_argument("--stateFile", "Path to the state file (e.g., result.json)", true);
-
-    // parse arguments
-    auto result = program.parse(argc, const_cast<const char**>(argv));
-    if (result) {
-        std::cerr << "Error: " << result.what() << std::endl;
-        program.print_help();
-        return 1;
-    }
-    std::string state_file = program.get<std::string>("--stateFile");
-    University rau;
-    rau.loadState(state_file);
-
-    if (program.exists("--addInstructor")) {
-        std::string instructorName = program.get<std::string>("addInstructor");
-        std::vector<Course> preferredCourses;
-        std::vector<TimeSlot> availability;
-
-        if (program.exists("--preferredCourses")) {
-            auto courses = program.get<std::vector<std::string>>("preferredCourses");
-            for (const auto& courseName : courses) {
-                Course* course = findCourseByName(courseName, rau);
-                if (course != nullptr) {
-                    preferredCourses.push_back(*course);
-                } else {
-                    std::cerr << "Course " << courseName << " not found!" << std::endl;
+            std::string instructorName = j["name"];
+            for (const auto& courseJson : j["preferred_courses"]) {
+                std::string courseName = courseJson["courseName"];
+                try {
+                    Course course = university.getCourse(courseName);
+                    preferredCourses.push_back(course);
+                } catch (const std::runtime_error& e) {
+                    std::cerr << "Error: " << e.what() << " for course " << courseName << std::endl;
                 }
             }
-        }
 
-        if (program.exists("--availability")) {
-            auto availabilityArgs = program.get<std::vector<std::string>>("availability");
-            for (size_t i = 0; i < availabilityArgs.size(); i += 3) {
-                TimeSlot slot(availabilityArgs[i], availabilityArgs[i + 1],
-                              availabilityArgs[i + 2]);
-                availability.push_back(slot);
+            for (const auto& slot : j["availability"]) {
+                TimeSlot timeSlot(slot["day"], slot["startTime"], slot["endTime"]);
+                availability.push_back(timeSlot);
             }
-        }
 
-        Instructor instructor(instructorName, availability, preferredCourses);
-        rau.addInstructor(instructor);
+            Instructor instructor(instructorName, availability, preferredCourses);
+            university.addInstructor(instructor);
+            university.saveState(stateFile);
 
-        rau.saveState("result.json");
-    }
+        } else if (arg == "--addCourse" && i + 1 < argc) {
+            university.loadState(stateFile);
+            std::string courseJson = argv[++i];
+            json j = json::parse(courseJson);
 
-    if (program.exists("--addCourse")) {
-        std::string courseName = program.get<std::string>("addCourse");
-        std::vector<TimeSlot> preferredTimeSlots;
-
-        if (program.exists("--preferredTimeSlots")) {
-            auto timeSlotArgs = program.get<std::vector<std::string>>("preferredTimeSlots");
-            for (size_t i = 0; i < timeSlotArgs.size(); i += 3) {
-                TimeSlot slot(timeSlotArgs[i], timeSlotArgs[i + 1], timeSlotArgs[i + 2]);
-                preferredTimeSlots.push_back(slot);
+            std::string courseName = j["name"];
+            std::vector<TimeSlot> preferredTimeSlots;
+            for (const auto& ts : j["preferred_time_slots"]) {
+                TimeSlot timeSlot(ts["day"], ts["startTime"], ts["endTime"]);
+                preferredTimeSlots.push_back(timeSlot);
             }
+
+            Course course(courseName, preferredTimeSlots);
+            std::cout << "COURSE: ";
+            course.displayInfo();
+            university.addCourse(course);
+            university.saveState(stateFile);
+
+        } else if (arg == "--addTimeSlot" && i + 1 < argc) {
+            university.loadState(stateFile);
+            std::string timeSlotArg = argv[++i];
+            json timeSlotJson = json::parse(timeSlotArg);
+
+            for (const auto& slot : timeSlotJson) {
+                TimeSlot timeSlot(slot["day"], slot["startTime"], slot["endTime"]);
+                university.addTimeSlot(timeSlot);
+            }
+            university.saveState(stateFile);
+
+        } else if (arg == "--schedule") {
+            university.loadState(stateFile);
+            auto schedule = university.schedule();
+            university.saveState(stateFile);
+
+            university.displaySchedule();
+
+        } else if (arg == "--clearFile") {
+            std::ofstream file(stateFile, std::ofstream::trunc);
+            if (file) {
+                json emptyJson = json::object();
+                file << emptyJson.dump(4);
+                file.close();
+                std::cout << "File cleared and initialized with an empty JSON object." << std::endl;
+            } else {
+                std::cerr << "Failed to open file " << stateFile << " for clearing." << std::endl;
+            }
+
+        } else {
+            std::cerr << "Unknown argument: " << arg << std::endl;
         }
-
-        Course course(courseName, preferredTimeSlots);
-        rau.addCourse(course);
-
-        rau.saveState("result.json");
     }
 
-    if (program.exists("--addTimeSlot")) {
-        auto timeSlotArgs = program.get<std::vector<std::string>>("addTimeSlot");
-        std::string day = timeSlotArgs[0];
-        std::string startTime = timeSlotArgs[1];
-        std::string endTime = timeSlotArgs[2];
-
-        TimeSlot timeSlot(day, startTime, endTime);
-        rau.addTimeSlot(timeSlot);
-
-        rau.saveState("result.json");
+    if (stateFile != "result.json" || std::find(argv, argv + argc, "--clearFile") == argv + argc) {
+        university.loadState(stateFile);
     }
+}
 
-    if (program.exists("--schedule")) {
-        rau.loadState("result.json");
-        auto schedule = rau.schedule();
-        rau.displaySchedule();
-        std::cout << "Schedule generated!" << std::endl;
-        return 0;
-    }
+int main(int argc, char** argv) {
+//    argparse::ArgumentParser program("UniversityTimetablingSystem", "University");
+//
+//    // define arguments
+//    program.add_argument("--addInstructor", "Add a new instructor", false);
+//
+//    program.add_argument("--addCourse", "Add a new course", false);
+//
+//    program.add_argument("--addTimeSlot", "Add a new time slot", false);
+//
+//    program.add_argument("--schedule", "Generate the schedule", false);
+//
+//    program.add_argument("--availability", "Represents instructor's availability", false);
+//
+//    program.add_argument("--preferredCourses", "Represents instructor's preferred courses", false);
+//
+//    program.add_argument("--preferredTimeSlots", "Represents preferred course time slots", false);
+//
+//    program.add_argument("--clearFile", "Clear the result.json file", false);
+//
+//    program.add_argument("--stateFile", "Path to the state file (e.g., result.json)", true);
+//
+//    // parse arguments
+//    auto result = program.parse(argc, const_cast<const char**>(argv));
+//    if (result) {
+//        std::cerr << "Error: " << result.what() << std::endl;
+//        program.print_help();
+//        return 1;
+//    }
+//    std::string state_file = program.get<std::string>("--stateFile");
+//    University rau;
+//    rau.loadState(state_file);
+//
+//    if (program.exists("--addInstructor")) {
+//        std::string instructorName = program.get<std::string>("addInstructor");
+//        std::vector<Course> preferredCourses;
+//        std::vector<TimeSlot> availability;
+//
+//        if (program.exists("--preferredCourses")) {
+//            auto courses = program.get<std::vector<std::string>>("preferredCourses");
+//            for (const auto& courseName : courses) {
+//                Course* course = findCourseByName(courseName, rau);
+//                if (course != nullptr) {
+//                    preferredCourses.push_back(*course);
+//                } else {
+//                    std::cerr << "Course " << courseName << " not found!" << std::endl;
+//                }
+//            }
+//        }
+//
+//        if (program.exists("--availability")) {
+//            auto availabilityArgs = program.get<std::vector<std::string>>("availability");
+//            for (size_t i = 0; i < availabilityArgs.size(); i += 3) {
+//                TimeSlot slot(availabilityArgs[i], availabilityArgs[i + 1],
+//                              availabilityArgs[i + 2]);
+//                availability.push_back(slot);
+//            }
+//        }
+//
+//        Instructor instructor(instructorName, availability, preferredCourses);
+//        rau.addInstructor(instructor);
+//
+//        rau.saveState("result.json");
+//    }
+//
+//    if (program.exists("--addCourse")) {
+//        std::string courseName = program.get<std::string>("addCourse");
+//        std::vector<TimeSlot> preferredTimeSlots;
+//
+//        if (program.exists("--preferredTimeSlots")) {
+//            auto timeSlotArgs = program.get<std::vector<std::string>>("preferredTimeSlots");
+//            for (size_t i = 0; i < timeSlotArgs.size(); i += 3) {
+//                TimeSlot slot(timeSlotArgs[i], timeSlotArgs[i + 1], timeSlotArgs[i + 2]);
+//                preferredTimeSlots.push_back(slot);
+//            }
+//        }
+//
+//        Course course(courseName, preferredTimeSlots);
+//        rau.addCourse(course);
+//
+//        rau.saveState("result.json");
+//    }
+//
+//    if (program.exists("--addTimeSlot")) {
+//        auto timeSlotArgs = program.get<std::vector<std::string>>("addTimeSlot");
+//        std::string day = timeSlotArgs[0];
+//        std::string startTime = timeSlotArgs[1];
+//        std::string endTime = timeSlotArgs[2];
+//
+//        TimeSlot timeSlot(day, startTime, endTime);
+//        rau.addTimeSlot(timeSlot);
+//
+//        rau.saveState("result.json");
+//    }
+//
+//    if (program.exists("--schedule")) {
+//        rau.loadState("result.json");
+//        auto schedule = rau.schedule();
+//        rau.displaySchedule();
+//        std::cout << "Schedule generated!" << std::endl;
+//        return 0;
+//    }
+//
+//    if (program.exists("--clearFile")) {
+//        std::ofstream file("result.json",
+//                           std::ofstream::trunc);  // we open the file in cleaning mode
+//        if (!file) {
+//            std::cerr << "Failed to open file result.json for clearing." << std::endl;
+//        }
+//
+//        json emptyJson = json::object();
+//        file << emptyJson.dump(4);
+//
+//        file.close();
+//        std::cout << "File result.json has been cleared and initialized with an empty JSON object."
+//                  << std::endl;
+//        rau.loadState("result.json");
+//    }
+//
+//    rau.displayInfo();
 
-    if (program.exists("--clearFile")) {
-        std::ofstream file("result.json",
-                           std::ofstream::trunc);  // we open the file in cleaning mode
-        if (!file) {
-            std::cerr << "Failed to open file result.json for clearing." << std::endl;
-        }
+    University university;
 
-        json emptyJson = json::object();
-        file << emptyJson.dump(4);
-
-        file.close();
-        std::cout << "File result.json has been cleared and initialized with an empty JSON object."
-                  << std::endl;
-        rau.loadState("result.json");
-    }
-
-    rau.displayInfo();
+    processArgs(argc, argv, university);
     return 0;
 }
